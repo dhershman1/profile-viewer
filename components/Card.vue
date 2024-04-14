e3dd2098954dfcf15501415a2e2a47dc8e31b2da
dhershman1 <dustinh17@gmail.com>
2024-04-14 16:06:35 -0400

    Added: Card component and styles

---
+++ components/Card.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card">
+    <section class="card__main">
+      <slot name="main" />
+    </section>
+    <section class="card__text">
+      <slot name="text" />
+    </section>
+  </div>
+</template>
