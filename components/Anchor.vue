3d9cca2b7863a077801882a25289e0cb36de627a
dhershman1 <dustinh17@gmail.com>
2024-04-14 10:57:20 -0400

    Added: Anchor Component

---
+++ components/Anchor.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ArrowUpRight } from 'lucide-vue-next'
+
+defineProps({
+  // text: {
+  //   type: String,
+  //   required: true
+  // },
+  href: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="link__wrapper">
+    <NuxtLink
+      :to="$props.href"
+      target="_blank"
+      class="link__anchor"
+    >
+      <slot></slot> <ArrowUpRight class="link__icon" />
+    </NuxtLink>
+  </div>
+</template>
