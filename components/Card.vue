eaf7e5778023fe71707a18f287ce1789bf4cf3a1
dhershman1 <dustinh17@gmail.com>
2024-04-14 19:25:43 -0400

    Added: Styles for repos

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,17 @@
+<script setup>
+defineProps({
+  hasActions: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
 <template>
   <div class="card">
+    <section v-if="$props.hasActions" class="card__actions">
+      <slot name="actions" />
+    </section>
     <section class="card__main">
       <slot name="main" />
     </section>
--- pages/user/[user].vue
+++ pages/user/[user].vue
@@ -1,5 +1,10 @@
 <script setup>
+import { fuzzySearch } from 'kyanite'
+import * as icons from 'vue3-simple-icons'
+import { Ban, Star, GitFork, CircleDot } from 'lucide-vue-next'
+
 const route = useRoute()
+const search = ref('')
 
 const profile = await useFetch(`https://api.github.com/users/${route.params.user}`, {
       pick: [
@@ -29,36 +34,76 @@ const repos = await useFetch(profile.data.value.repos_url, {
     per_page: profile.data.value.public_repos
   }
 })
+
+const filteredRepos = computed(() => {
+  return repos.data.value.filter(({ name }) => fuzzySearch(search.value, name))
+})
+
+function getIcon (lang) {
+  // This isn't a great function, we need a better way to dynamically grab icons from simple icons
+  if (!lang) {
+    return Ban
+  }
+
+  switch (lang.toLowerCase()) {
+    case 'html':
+      return icons['Html5Icon']
+    case 'css':
+      return icons['Css3Icon']
+    case 'vue':
+      return icons['VueDotjsIcon']
+    default:
+      return icons[`${lang}Icon`]
+  }
+}
 </script>
 
 <template>
   <div class="user">
-    <section>
+    <section class="user__profile">
       <Profile :profile="profile.data.value" />
     </section>
-    <section>
-      <div class="filters">
-
+    <section class="repositories">
+      <div class="repos">
+        <Card has-actions>
+          <template #actions>
+            <div class="filters">
+              <section class="filters__search">
+                <input
+                  class="filters__input"
+                  type="text"
+                  v-model="search"
+                />
+              </section>
+            </div>
+          </template>
+          <template #main>
+            <ul class="repos__list">
+              <li
+                class="repos__item"
+                v-for="repo in filteredRepos"
+                :key="repo.id"
+              >
+                <section class="repo">
+                  <Anchor class="repo__link" :href="repo.html_url">
+                    {{ repo.name }}
+                  </Anchor>
+                  <p class="repo__description">{{ repo.description }}</p>
+                  <p class="repo__lang">
+                    <component :is="getIcon(repo.language)" />
+                    {{ repo.language }}
+                  </p>
+                </section>
+                <section class="stats">
+                  <p><Star /> {{ repo.stargazers_count }} Stars</p>
+                  <p><GitFork/> {{ repo.forks_count }} Forks</p>
+                  <p><CircleDot /> {{ repo.open_issues }} Issues</p>
+                </section>
+              </li>
+            </ul>
+          </template>
+        </Card>
       </div>
-      <ol>
-        <li v-for="repo in repos.data.value" :key="repo.id">
-          {{ repo.name }}
-          <ul>
-            <li>
-              {{ repo.description }}
-            </li>
-            <li>
-              Stars: {{ repo.stargazers_count }}
-            </li>
-            <li>
-              Issues: {{ repo.open_issues_count }}
-            </li>
-            <li>
-              Forks: {{ repo.forks_count }}
-            </li>
-          </ul>
-        </li>
-      </ol>
     </section>
   </div>
 </template>
@@ -72,9 +117,75 @@ const repos = await useFetch(profile.data.value.repos_url, {
   grid-template-columns: 30% 70%;
 }
 
-.user section {
+.user .user__profile {
   position: sticky;
   top: 0;
+}
+
+.repositories {
+  margin-right: 0.5rem;
+}
+
+.repos__list {
+  list-style: none;
+}
+
+.repos__item {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border-bottom: 1px solid var(--lightgrey);
+  margin-top: 0.5rem;
+}
+
+.repos__item:first-child {
+  padding-top: 0.5rem;
+  border-top: 1px solid var(--lightgrey);
+}
+
+.repos__item:last-child {
+  border-bottom: none;
+}
+
+.repo__link {
+  font-size: 20px;
+  font-weight: 500;
+}
+
+.repo__description {
+  font-size: 14px;
+}
+
+.repo__lang {
+  display: flex;
+  gap: 0.2rem;
+  font-size: 12px;
+}
+
+.repo__lang svg {
+  width: 14px;
+  height: 14px;
+}
+
+.stats {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  border-left: 1px solid var(--lightgrey);
+}
+
+.stats p {
+  vertical-align: middle;
+  font-size: 16px;
+  margin: 0;
+  display: flex;
+  align-items: center;
+}
+
+.stats p svg {
+  width: 16px;
+  height: 16px;
+  margin-left: 0.2rem;
+  margin-right: 0.2rem;
 }
 
 @media only screen and (max-width: 768px) {
@@ -82,8 +193,12 @@ const repos = await useFetch(profile.data.value.repos_url, {
     grid-template-columns: 1fr;
   }
 
-  .user section {
+  .user .user__profile {
     position: inherit;
+  }
+
+  .repositories {
+    width: 100%;
   }
 }
 </style>
