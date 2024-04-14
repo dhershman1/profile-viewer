957afcbfdc3f38301ad88a59fb7b1b6a4c3acd89
dhershman1 <dustinh17@gmail.com>
2024-04-14 07:54:00 -0400

    initial commit

---
+++ components/Search.vue
@@ -0,0 +1,26 @@
+<script setup>
+const userSearch = ref('')
+const userData = ref({})
+
+async function search () {
+  console.log(userSearch.value)
+
+  const data = await $fetch(`https://api.github.com/users/${userSearch.value}`)
+
+  userData.value = data
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="search">
+      <input v-model="userSearch" type="text" />
+      <button>Search</button>
+    </form>
+    <pre>
+      {{ userData }}
+    </pre>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/[user].vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>User</h1>
+    <h2>{{ $route.params.user }}</h2>
+  </div>
+</template>
