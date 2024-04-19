3121d87dfb829be40d7950aea4ae397a3d1bca61
dhershman1 <dustinh17@gmail.com>
2024-04-19 17:47:00 -0400

    Added: Loading setup for page transitions

--- app.vue
+++ app.vue
@@ -1,17 +1,30 @@
 <script setup>
+const nuxtApp = useNuxtApp()
+const loading = ref(false)
+
+nuxtApp.hook('page:start', () => {
+  loading.value = true;
+})
+
+nuxtApp.hook('page:finish', () => {
+  loading.value = false
+})
+
 useHead({
   titleTemplate: titleChunk => {
-    return titleChunk ? `${titleChunk} - Profile Viewer` : 'Profile Viewer'
+    return titleChunk ? `Profile Viewer - ${titleChunk}` : 'Profile Viewer'
   }
 })
 </script>
 
 <template>
   <div>
+    <NuxtLoadingIndicator />
     <nav>
       <NuxtLink to="/">Github Profile Viewer</NuxtLink>
       <Search />
     </nav>
+    <div v-if="loading" class="loader"></div>
     <NuxtPage />
   </div>
 </template>
